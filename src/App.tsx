7dce04ca272e2faa8f84f960567b261dc8bb8fd4
Noa Giannone <noa.giannone@gmail.com>
2025-11-30 20:24:31 +0100

    refactor: remove unused Adhesion import from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { Dashboard } from './app/pages/Dashboard';
 import { Profile } from './app/pages/Profile';
 import { QRCodeSuccess } from './app/pages/QRCodeSuccess';
 import { CheckIn } from './app/pages/CheckIn';
-import { Adhesion } from './app/pages/Adhesion';
 import { AdminRoutes } from './admin/routes';
 import { AdminAuthProvider } from './shared/contexts/AdminAuthContext';
 import { EventScannerPage } from './admin/pages/Scanner/EventScannerPage';
@@ -45,9 +44,8 @@ function App() {
             }
           />
 
-          {/* Routes sans navbar (inscription, adhésion et QR login) */}
+          {/* Routes sans navbar (inscription et QR login) */}
           <Route path="/signup" element={<EnhancedSignup />} />
-          <Route path="/adhesion" element={<Adhesion />} />
           <Route path="/qr-login" element={<QRLogin />} />
           <Route path="/signup-success" element={
             <ProtectedRoute>
